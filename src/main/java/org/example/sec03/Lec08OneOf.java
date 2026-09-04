ea23f2f0762fa75972b682d096e636c9c7cae2b4
Pablo Santos <pablo.santos@classora.com>
2026-09-04 08:32:54 +0200

    one_of

---
+++ src/main/java/org/example/sec03/Lec08OneOf.java
@@ -0,0 +1,30 @@
+package org.example.sec03;
+
+import com.example.models.sec03.Credentials;
+import com.example.models.sec03.Email;
+import com.example.models.sec03.Phone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Lec08OneOf {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec08OneOf.class);
+
+    public static void main(String[] args) {
+
+        var email = Email.newBuilder().setAddress("sam@gmail.com").setPassword("admin").build();
+        var phone = Phone.newBuilder().setNumber(123456789).setCode(123).build();
+
+        //login(Credentials.newBuilder().setEmail(email).build());
+        //login(Credentials.newBuilder().setPhone(phone).build());
+        login(Credentials.newBuilder().setEmail(email).setPhone(phone).build());
+    }
+
+    private static void login(Credentials credentials) {
+        switch (credentials.getLoginTypeCase()) {
+            case EMAIL -> log.info("email -> {}", credentials.getEmail());
+            case PHONE -> log.info("phone -> {}", credentials.getPhone());
+        }
+    }
+}
