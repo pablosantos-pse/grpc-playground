b99d6c9372a6dd30775aa8b223a847c4ab1837d5
Pablo Santos <pablo.santos@classora.com>
2026-09-06 12:16:01 +0200

    async_stub

--- src/main/java/org/example/sec06/GrpcClient.java
+++ src/main/java/org/example/sec06/GrpcClient.java
@@ -1,25 +1,46 @@
 package org.example.sec06;
 
+import com.example.models.sec06.AccountBalance;
 import com.example.models.sec06.BalanceCheckRequest;
 import com.example.models.sec06.BankServiceGrpc;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
 
 public class GrpcClient {
 
     private static final Logger log = LoggerFactory.getLogger(GrpcClient.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         var channel = ManagedChannelBuilder.forAddress("localhost", 6565)
                 .usePlaintext()
                 .build();
 
-        var stub = BankServiceGrpc.newBlockingStub(channel);
+        var stub = BankServiceGrpc.newStub(channel);
 
-        var balance = stub.getAccountBalance(BalanceCheckRequest.newBuilder().setAccountNumber(2).build());
+        stub.getAccountBalance(BalanceCheckRequest.newBuilder().setAccountNumber(2).build(), new StreamObserver<AccountBalance>() {
+            @Override
+            public void onNext(AccountBalance accountBalance) {
+                log.info("{}", accountBalance);
+            }
 
-        log.info("{}", balance);
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                log.info("completed");
+            }
+        });
+
+        log.info("done");
+        Thread.sleep(Duration.ofSeconds(1));
+
     }
 }
